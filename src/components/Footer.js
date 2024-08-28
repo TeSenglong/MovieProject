9a84d5b5d1c5689baabb57f0ddf6ddab612063db
seng long <hahura70@gmail.com>
2024-08-28 21:55:15 +0700

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -83,7 +83,7 @@ export default function Footer() {
                     </ul>
                 </article>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-center text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+                <span className="block text-center text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 web20 Istad All Rights Reserved.</span>
             </div>
         </footer>
 
